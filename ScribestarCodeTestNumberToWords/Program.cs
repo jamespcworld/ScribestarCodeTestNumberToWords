c936c266731d401571003148c9727eabeabe4002
James <James@DESKTOP-9DPKHEF>
2019-01-27 23:15:27 +0000

    Add project files.

---
+++ ScribestarCodeTestNumberToWords/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ScribestarCodeTestNumberToWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
